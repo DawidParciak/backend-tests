bd9d19e6db1c668467876402c7d4a55406f2c862
Dawid <daw.parc@gmail.com>
2023-05-11 17:51:49 +0200

    Change 'to.equal' to 'to.be.equal'

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -4,31 +4,31 @@ const expect = require('chai').expect;
 describe('formatFullname', () => {
 
   it('should return formatted name when given valid input', () => {
-    expect(formatFullname('john doe')).to.equal('John Doe');
-    expect(formatFullname('JOhN DoE')).to.equal('John Doe');
-    expect(formatFullname('amanda doe')).to.equal('Amanda Doe');
-    expect(formatFullname('JOHN DOE')).to.equal('John Doe');
-    expect(formatFullname('JOHN doE')).to.equal('John Doe');
+    expect(formatFullname('john doe')).to.be.equal('John Doe');
+    expect(formatFullname('JOhN DoE')).to.be.equal('John Doe');
+    expect(formatFullname('amanda doe')).to.be.equal('Amanda Doe');
+    expect(formatFullname('JOHN DOE')).to.be.equal('John Doe');
+    expect(formatFullname('JOHN doE')).to.be.equal('John Doe');
   });
 
   it('should return "Error" when input is empty', () => {
-    expect(formatFullname('')).to.equal('Error');
+    expect(formatFullname('')).to.be.equal('Error');
   });
 
   it('should return "Error" when input is not a string', () => {
-    expect(formatFullname(null)).to.equal('Error');
-    expect(formatFullname(undefined)).to.equal('Error');
-    expect(formatFullname(123)).to.equal('Error');
-    expect(formatFullname(['john doe'])).to.equal('Error');
-    expect(formatFullname({fullName: 'john doe'})).to.equal('Error');
+    expect(formatFullname(null)).to.be.equal('Error');
+    expect(formatFullname(undefined)).to.be.equal('Error');
+    expect(formatFullname(123)).to.be.equal('Error');
+    expect(formatFullname(['john doe'])).to.be.equal('Error');
+    expect(formatFullname({fullName: 'john doe'})).to.be.equal('Error');
   });
 
   it('should return "Error" when input has incorrect format', () => {
-    expect(formatFullname('john')).to.equal('Error');
-    expect(formatFullname('doe')).to.equal('Error');
-    expect(formatFullname('John Doe Test')).to.equal('Error');
-    expect(formatFullname('John Test Doe')).to.equal('Error');
-    expect(formatFullname('John ')).to.equal('Error');
-    expect(formatFullname(' Doe')).to.equal('Error');
+    expect(formatFullname('john')).to.be.equal('Error');
+    expect(formatFullname('doe')).to.be.equal('Error');
+    expect(formatFullname('John Doe Test')).to.be.equal('Error');
+    expect(formatFullname('John Test Doe')).to.be.equal('Error');
+    expect(formatFullname('John ')).to.be.equal('Error');
+    expect(formatFullname(' Doe')).to.be.equal('Error');
   });
 });
